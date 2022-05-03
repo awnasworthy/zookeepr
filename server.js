ab179d4a6b24105a1060bccdf5d63b3af6446baa
awnasworthy <allyson.nasworthy@gmail.com>
2022-05-03 10:54:59 -0400

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const { animals } = require('./data/animals.json');
+
+const PORT = 3001;
+
+const app = express();
+
+app.get('/api/animals', (req, res) => {
+    res.json(animals);
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port ${PORT}`);
+});
